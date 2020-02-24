6aa73943a83d92315715c106dec0462e671b2703
Ric Az <ricardoazevedo96@gmail.com>
2020-02-24 14:28:32 +0000

    channels rotes

--- routes/channels.js
+++ routes/channels.js
@@ -2,20 +2,49 @@
 
 const { Router } = require('express');
 
-
-
 const router = new Router();
-
+// channel routes
 router.get('/', (req, res, next) => {
   res.render('channels');
 });
 
+router.get('/create', (req, res, next) => {
+  res.render('channels/create');
+});
+router.post('/create', (req, res, next) => {
+  const { id } = req.body;
+  res.redirect('channels/:channel_id');
+});
 
-router.get('/create', (req, res, next) =>{
-  res.render('channels/create')
-})
+router.get('/:channel_id', (req, res, next) => {
+  const { id } = req.body;
 
-// router.get('/:channel_id', req, res, next) => {
-//   res.render('channels/singleview')
-// })
+  res.render('channels/singleview');
+});
+router.get('/:channel_id/edit', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/edit');
+});
+
+router.get('/:channel_id/edit', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/edit');
+});
+router.post('/:channel_id/edit', (req, res, next) => {
+  const { id } = req.body;
+  res.redirect('channels/:channel_id');
+});
+
+// posts routes
+
+router.get('/:channel_id/:post_id', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/post/singlepost');
+});
+
+router.get('/:channel_id/:', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/post/singlepost');
+});
+
 module.exports = router;
