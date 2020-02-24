a23bc61a70b5be1fc92f10837bfd1dcdf459116d
Ric Az <ricardoazevedo96@gmail.com>
2020-02-24 16:14:51 +0000

    critical update

--- multer-configure.js
+++ multer-configure.js
@@ -3,17 +3,19 @@ const cloudinary = require('cloudinary');
 const multerStorageCloudinary = require('multer-storage-cloudinary');
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_API_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
-	resource_type: 'raw'
+  cloud_name: process.env.CLOUDINARY_API_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  resource_type: 'raw'
 });
 
 const storage = multerStorageCloudinary({
-	cloudinary,
-	folder: 'jan20',
-	resource_type: 'raw',
-	allowedFormats: ['jpg', 'png', 'mov', 'mp4']
+  cloudinary,
+  folder: 'jan20',
+  resource_type: 'raw',
+  allowedFormats: ['jpg', 'png', 'mov', 'mp4']
 });
 
 const uploader = multer({ storage });
+
+module.exports = uploader;
