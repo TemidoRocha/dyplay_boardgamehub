b81a791a643ab053d0004d4bb9fa90112a2b4f86
Ric Az <ricardoazevedo96@gmail.com>
2020-02-24 16:05:32 +0000

    bug fixed

--- routes/channels.js
+++ routes/channels.js
@@ -14,29 +14,29 @@ router.get('/:channel_id/create_post', (req, res, next) => {
   const { id } = req.body;
   res.render('channels/post/create');
 });
-router.post('/:channel_id/create_post', uploader.single('picture'), (req, res, next) => {
-  const { title, content } = req.body;
-  const { channel_id } = req.params;
+// router.post('/:channel_id/create_post', uploader.single('picture'), (req, res, next) => {
+//   const { title, content } = req.body;
+//   const { channel_id } = req.params;
 
-  const { url } = req.files;
+//   const { url } = req.files;
 
-  const author = req.user._id;
+//   const author = req.user._id;
 
-  Post.create({
-    title,
-    content,
-    channel: channel_id,
-    author,
-    photos: url
-  })
-    .then(post => {
-      res.redirect(`/channel/${post.channel}/post/${post._id}`);
-    })
-    .catch(error => {
-      next(error);
-    });
-  res.redirect('channels/:post_id/singlepost');
-});
+//   Post.create({
+//     title,
+//     content,
+//     channel: channel_id,
+//     author,
+//     photos: url
+//   })
+//     .then(post => {
+//       res.redirect(`/channel/${post.channel}/post/${post._id}`);
+//     })
+//     .catch(error => {
+//       next(error);
+//     });
+//   res.redirect('channels/:post_id/singlepost');
+// });
 router.get('/:channel_id/:post_id', (req, res, next) => {
   const { id } = req.body;
   res.render('channels/post/singlepost');
@@ -60,23 +60,23 @@ router.get('/create', (req, res, next) => {
   res.render('channels/create');
 });
 
-router.post('/create', uploader.single('picture'), (req, res, next) => {
-  const author = req.user._id;
-  const { name, desciption } = req.body;
-  const { url } = req.files;
-  Channel.create({
-    name,
-    desciption,
-    picture: url,
-    author
-  })
-    .then(channel => {
-      res.redirect(`/channel/${channel._id}`);
-    })
-    .catch(error => {
-      next(error);
-    });
-});
+// router.post('/create', uploader.single('picture'), (req, res, next) => {
+//   const author = req.user._id;
+//   const { name, desciption } = req.body;
+//   const { url } = req.files;
+//   Channel.create({
+//     name,
+//     desciption,
+//     picture: url,
+//     author
+//   })
+//     .then(channel => {
+//       res.redirect(`/channel/${channel._id}`);
+//     })
+//     .catch(error => {
+//       next(error);
+//     });
+// });
 
 router.get('/:channel_id', (req, res, next) => {
   const { id } = req.body;
