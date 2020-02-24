b72776afdda1b97f42172532ea8b6f7fc28aff51
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-02-24 15:44:20 +0000

    multer and passaport config

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,38 +1,38 @@
 'use strict';
 
 const { Router } = require('express');
+const router = new Router();
 
 const passport = require('passport');
 
-const router = new Router();
-
-router.get('/sign-up', (req, res, next) => {
-  res.render('sign-up');
+router.get('/sign-in', (req, res, next) => {
+	res.render('sign-in');
 });
 
+
 router.post(
-  '/sign-up',
-  passport.authenticate('local-sign-up', {
-    successRedirect: '/private',
-    failureRedirect: '/sign-up'
-  })
+	'/sign-in',
+	passport.authenticate('sign-in', {
+		successRedirect: '/',
+		failureRedirect: 'sign-in'
+	})
 );
 
-router.get('/sign-in', (req, res, next) => {
-  res.render('sign-in');
+router.get('/sign-up', (req, res, next) => {
+	res.render('sign-up');
 });
 
 router.post(
-  '/sign-in',
-  passport.authenticate('local-sign-in', {
-    successRedirect: '/private',
-    failureRedirect: '/sign-in'
-  })
+	'/sign-up',
+	passport.authenticate('sign-up', {
+		successRedirect: '/',
+		failureRedirect: 'sign-up'
+	})
 );
 
 router.post('/sign-out', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
 
 module.exports = router;
