e474549e43ce7c3f6b871ea96e3eba3c900dd9d3
Ric Az <ricardoazevedo96@gmail.com>
2020-02-24 15:00:07 +0000

    working in routes

--- routes/channels.js
+++ routes/channels.js
@@ -3,6 +3,33 @@
 const { Router } = require('express');
 
 const router = new Router();
+
+const Channel = require('./../models/channel');
+
+// posts routes
+
+router.get('/:channel_id/create_post', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/post/create');
+});
+router.post('/:channel_id/create_post', (req, res, next) => {
+  const { id } = req.body;
+  res.redirect('channels/:post_id/singlepost');
+});
+router.get('/:channel_id/:post_id', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/post/singlepost');
+});
+
+router.get('/:channel_id/:post:id/edit', (req, res, next) => {
+  const { id } = req.body;
+  res.render('channels/post/edit');
+});
+router.post('/:channel_id/:post:id/edit', (req, res, next) => {
+  const { id } = req.body;
+  res.redirect('channels/:post_id/singlepost');
+});
+
 // channel routes
 router.get('/', (req, res, next) => {
   res.render('channels');
@@ -12,8 +39,16 @@ router.get('/create', (req, res, next) => {
   res.render('channels/create');
 });
 router.post('/create', (req, res, next) => {
-  const { id } = req.body;
-  res.redirect('channels/:channel_id');
+  const { name } = req.body;
+  Channel.create({
+    name
+  })
+    .then(channel => {
+      res.redirect(`/channel/${channel._id}`);
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 router.get('/:channel_id', (req, res, next) => {
@@ -33,18 +68,6 @@ router.get('/:channel_id/edit', (req, res, next) => {
 router.post('/:channel_id/edit', (req, res, next) => {
   const { id } = req.body;
   res.redirect('channels/:channel_id');
-});
-
-// posts routes
-
-router.get('/:channel_id/:post_id', (req, res, next) => {
-  const { id } = req.body;
-  res.render('channels/post/singlepost');
-});
-
-router.get('/:channel_id/:', (req, res, next) => {
-  const { id } = req.body;
-  res.render('channels/post/singlepost');
 });
 
 module.exports = router;
