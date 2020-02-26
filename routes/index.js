9f635c6f35630d0c70c460eb097dbf479db24550
Ric Az <ricardoazevedo96@gmail.com>
2020-02-26 11:45:09 +0000

    partials for trending

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@
 const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
+const Post = require('./../models/post');
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+  Post.find()
+    .sort({ timestamp: 'descending' })
+    .limit(2)
+    .then(posts => {
+      res.render('index', { posts });
+    });
 });
 
 router.get('/private', routeGuard, (req, res, next) => {
