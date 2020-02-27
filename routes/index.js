91a23f2f503c03fca90e2581df3fbe240e7ff0ef
TemidoRocha <manueltemidorocha@gmail.com>
2020-02-27 19:12:54 +0000

    css encounters

--- routes/index.js
+++ routes/index.js
@@ -9,46 +9,72 @@ const gameList = require('./../variables');
 const User = require('./../models/user');
 
 router.get('/', (req, res, next) => {
-	let postSide;
-	let events;
-	let eventsSide;
+  let postSide;
+  let events;
+  let eventsSide;
 
-	Post.find()
-		.sort({ timestamp: 'descending' })
-		.limit(4)
-		.then(documents => {
-			postSide = documents;
-			return Event.find();
-		})
-		.then(document => {
-			events = document;
-		})
-		.then(() => {
-			return Event.find()
-				.sort({ creationDate: 'descending' })
-				.limit(5);
-		})
-		.then(document => {
-			eventsSide = document;
-			res.render('index', { postSide, events, eventsSide });
-		})
-		.catch(error => {
-			next(error);
-		});
+  Post.find()
+    .sort({ timestamp: 'descending' })
+    .limit(4)
+    .then(documents => {
+      postSide = documents;
+      return Event.find();
+    })
+    .then(document => {
+      events = document;
+    })
+    .then(() => {
+      return Event.find()
+        .sort({ creationDate: 'descending' })
+        .limit(5);
+    })
+    .then(document => {
+      eventsSide = document;
+      res.render('index', { postSide, events, eventsSide });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 router.get('/profile/:id', routeGuard, (req, res, next) => {
-	const { id } = req.params;
-	res.render('userPublic');
+  let postSide;
+  let eventsSide;
+  Post.find()
+    .sort({ timestamp: 'descending' })
+    .limit(2)
+    .then(documents => {
+      postSide = documents;
+      return Event.find()
+        .sort({ creationDate: 'descending' })
+        .limit(3);
+    })
+    .then(something => {
+      eventsSide = something;
+      res.render('userPublic', { eventsSide, postSide });
+    });
 });
 
 router.get('/edit', routeGuard, (req, res, next) => {
-	console.log(req.user);
-	res.render('edit', { gameList });
+  let postSide;
+  let eventsSide;
+  Post.find()
+    .sort({ timestamp: 'descending' })
+    .limit(2)
+    .then(documents => {
+      postSide = documents;
+      return Event.find()
+        .sort({ creationDate: 'descending' })
+        .limit(3);
+    })
+    .then(something => {
+      eventsSide = something;
+      res.render('edit', { gameList, eventsSide, postSide });
+    });
 });
 
 router.post('/edit', routeGuard, (req, res, next) => {
-	res.redirect('/user');
+  res.redirect('/user');
 });
 
 module.exports = router;
