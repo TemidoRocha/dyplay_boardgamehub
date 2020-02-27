99634373d544ce7e8af8c56426777714dfa3230f
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-02-27 11:55:23 +0000

    userPublic page done

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,40 @@ const Post = require('./../models/post');
 const Event = require('./../models/event');
 
 router.get('/', (req, res, next) => {
-  let postSide;
-  let events;
-  let eventsSide;
-  Post.find()
-    .sort({ timestamp: 'descending' })
-    .limit(2)
-    .then(documents => {
-      postSide = documents;
-      return Event.find();
-    })
-    .then(document => {
-      events = document;
-    })
-    .then(() => {
-      return Event.find()
-        .sort({ creationDate: 'descending' })
-        .limit(3);
-    })
-    .then(document => {
-      eventsSide = document;
-      res.render('index', { postSide, events, eventsSide });
-    })
-    .catch(error => {
-      next(error);
-    });
+	let postSide;
+	let events;
+	let eventsSide;
+	Post.find()
+		.sort({ timestamp: 'descending' })
+		.limit(2)
+		.then(documents => {
+			postSide = documents;
+			return Event.find();
+		})
+		.then(document => {
+			events = document;
+		})
+		.then(() => {
+			return Event.find()
+				.sort({ creationDate: 'descending' })
+				.limit(3);
+		})
+		.then(document => {
+			eventsSide = document;
+			res.render('index', { postSide, events, eventsSide });
+		})
+		.catch(error => {
+			next(error);
+		});
+});
+
+router.get('/profile/:id', routeGuard, (req, res, next) => {
+	const { id } = req.params;
+	res.render('userPublic');
 });
 
 router.get('/private', routeGuard, (req, res, next) => {
-  console.log(req.user.location)
-  res.render('private');
+	res.render('private');
 });
 
 module.exports = router;
