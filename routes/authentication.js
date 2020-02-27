031f0567902a914c182b6a6f9f19b809de06ab33
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-02-27 17:32:14 +0000

    edit bug fix

--- routes/authentication.js
+++ routes/authentication.js
@@ -10,70 +10,82 @@ const User = require('./../models/user');
 //for the nodemailer
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASSWORD
-  }
+	service: 'Gmail',
+	auth: {
+		user: process.env.EMAIL,
+		pass: process.env.PASSWORD
+	}
 });
 
 const passport = require('passport');
 
 router.get('/sign-in', (req, res, next) => {
-  res.render('sign-in');
+	res.render('sign-in');
 });
 
 router.post(
-  '/sign-in',
-  passport.authenticate('sign-in', {
-    successRedirect: '/',
-    failureRedirect: 'sign-in'
-  })
+	'/sign-in',
+	passport.authenticate('sign-in', {
+		successRedirect: '/',
+		failureRedirect: 'sign-in'
+	})
 );
 
 router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 router.get(
-  '/google-callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect('/');
-  }
+	'/google-callback',
+	passport.authenticate('google', { failureRedirect: '/' }),
+	(req, res) => {
+		res.redirect('/');
+	}
 );
 
 router.get('/sign-up', (req, res, next) => {
-  res.render('sign-up', { gameList });
+	res.render('sign-up', { gameList });
 });
 
 router.post(
-  '/sign-up',
-  uploader.single('picture'),
-  passport.authenticate('sign-up', {
-    successRedirect: '/',
-    failureRedirect: 'sign-up'
-  })
+	'/sign-up',
+	uploader.single('picture'),
+	passport.authenticate('sign-up', {
+		successRedirect: '/',
+		failureRedirect: 'sign-up'
+	})
+);
+
+router.get('/edit', (req, res, next) => {
+	res.render('edit', { gameList });
+});
+router.post(
+	'/edit',
+	uploader.single('picture'),
+	passport.authenticate('edit', {
+		successRedirect: '/',
+		failureRedirect: 'edit'
+	})
 );
 
 router.post('/sign-out', (req, res, next) => {
-  req.logout();
-  res.redirect('/');
+	req.logout();
+	res.redirect('/');
 });
 
 router.post('/deleteAccount', routeGuard, (req, res, next) => {
-  transporter
-    .sendMail({
-      from: `DypPlay BoarGameHub <${process.env.EMAIL}>`,
-      to: req.user.email,
-      subject: 'DyPlay says Goodbye',
-      // text: 'Hello world!'
-      html: 'DyPlays says farewell! We hope this is not a goodbye!'
-    })
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  User.deleteOne({ _id: req.user._id }).then(() => res.redirect('/'));
+	transporter
+		.sendMail({
+			from: `DyPlay BoarGameHub <${process.env.EMAIL}>`,
+			to: req.user.email,
+			subject: 'DyPlay says Goodbye',
+			// text: 'Hello world!'
+			html: 'DyPlays says farewell! We hope this is not a goodbye!'
+		})
+		.then(result => {
+			console.log(result);
+		})
+		.catch(error => {
+			console.log(error);
+		});
+	User.deleteOne({ _id: req.user._id }).then(() => res.redirect('/'));
 });
 
 module.exports = router;
