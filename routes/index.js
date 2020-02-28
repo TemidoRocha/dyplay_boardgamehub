a5db12d0e76e51effb6803aa7e8a318c138306cc
Matheus Carvalho <carvalho.bfr@gmail.com>
2020-02-28 12:31:02 +0000

    click in other user profile done

--- routes/index.js
+++ routes/index.js
@@ -11,72 +11,84 @@ const uploader = require('./../multer-configure.js');
 const passport = require('passport');
 
 router.get('/', (req, res, next) => {
-  let postSide;
-  let events;
-  let eventsSide;
+	let postSide;
+	let events;
+	let eventsSide;
 
-  Post.find()
-    .sort({ timestamp: 'descending' })
-    .limit(4)
-    .then(documents => {
-      postSide = documents;
-      return Event.find();
-    })
-    .then(document => {
-      events = document;
-    })
-    .then(() => {
-      return Event.find()
-        .sort({ creationDate: 'descending' })
-        .limit(5);
-    })
-    .then(document => {
-      eventsSide = document;
-      res.render('index', { postSide, events, eventsSide });
-    })
-    .catch(error => {
-      next(error);
-    });
+	Post.find()
+		.sort({ timestamp: 'descending' })
+		.limit(4)
+		.then(documents => {
+			postSide = documents;
+			return Event.find();
+		})
+		.then(document => {
+			events = document;
+		})
+		.then(() => {
+			return Event.find()
+				.sort({ creationDate: 'descending' })
+				.limit(5);
+		})
+		.then(document => {
+			eventsSide = document;
+			res.render('index', { postSide, events, eventsSide });
+		})
+		.catch(error => {
+			next(error);
+		});
 });
 
 router.get('/profile/:id', routeGuard, (req, res, next) => {
-  let postSide;
-  let eventsSide;
-  Post.find()
-    .sort({ timestamp: 'descending' })
-    .limit(2)
-    .then(documents => {
-      postSide = documents;
-      return Event.find()
-        .sort({ creationDate: 'descending' })
-        .limit(3);
-    })
-    .then(something => {
-      eventsSide = something;
-      res.render('userPublic', { eventsSide, postSide });
-    });
+	let postSide;
+	let eventsSide;
+	let userdata;
+
+	User.findById(req.params.id)
+		.then(theUser => {
+			userdata = theUser;
+			return Post.find()
+				.sort({ timestamp: 'descending' })
+				.limit(2);
+		})
+		.then(posts => {
+			postSide = posts;
+			return Event.find()
+				.sort({ creationDate: 'descending' })
+				.limit(3);
+		})
+		.then(event => {
+			eventsSide = event;
+			res.render('userPublic', { userdata, eventsSide, postSide });
+		})
+		.catch(error => {
+			console.log(error);
+			next(error);
+		});
+
+	// res.render('userPublic', data);
 });
 
 router.get('/edit', routeGuard, (req, res, next) => {
-  let postSide;
-  let eventsSide;
-  Post.find()
-    .sort({ timestamp: 'descending' })
-    .limit(2)
-    .then(documents => {
-      postSide = documents;
-      return Event.find()
-        .sort({ creationDate: 'descending' })
-        .limit(3);
-    })
-    .then(something => {
-      eventsSide = something;
-      res.render('edit', { gameList, eventsSide, postSide });
-    });
+	let postSide;
+	let eventsSide;
+	Post.find()
+		.sort({ timestamp: 'descending' })
+		.limit(2)
+		.then(documents => {
+			postSide = documents;
+			return Event.find()
+				.sort({ creationDate: 'descending' })
+				.limit(3);
+		})
+		.then(something => {
+			eventsSide = something;
+			res.render('edit', { gameList, eventsSide, postSide });
+		});
 });
 
 router.post('/edit', routeGuard, (req, res, next) => {
-  res.redirect('/user');
+	res.redirect('/user');
 });
 
 module.exports = router;
